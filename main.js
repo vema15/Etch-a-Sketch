9c7d6a1a83cabf09f7d8e3923597cf48327e581d
vema15 <140360041+vema15@users.noreply.github.com>
2023-10-17 18:07:03 -0700

    Created function that grabs user input. Created another function to absorb the user input and create a grid of user's choice. Grid confined to 100x100. Bug warning: Grid does not reset unless page is refreshed (working on this).

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
+let val = document.getElementById('dimension-input');
 
+
+let valInputBtn = document.getElementById('dimension-input-btn');
+valInputBtn.addEventListener('click', function () {
+    if(val.value <= 100) {
+        gridLW = val.value;
+    } else {
+        gridLW = 100;
+        alert('Inputted Values may not exceed 100. This is a 100x100 grid.')
+    }
+    
+    
+    createGrid(gridLW);
+    function createGrid(gridLW) {
     const gridContainer = document.querySelector('.grid-container');
-    for (let i = 0; i <=16; i++) {
+    for (let i = 0; i <= gridLW-1; i++) {
         const rowDivs = document.createElement('div');
         rowDivs.className = 'rowDivs';
         gridContainer.appendChild(rowDivs);
 
-        for(let j = 0; j <= 16; j++) {
+        for (let j = 0; j <= gridLW-1; j++) {
             const gridDivs = document.createElement('div');
             gridDivs.className = 'gridDivs';
-            gridDivs.setAttribute('id', 'rowDivs');
-            gridDivs.addEventListener('drag', function() {
-                gridDivs.className = 'gridDivsHover';
-            }
-            );
+            gridDivs.setAttribute(`style`, `width: ${500/gridLW}px; height: ${500/gridLW}px`);
             rowDivs.appendChild(gridDivs);
-            
+
+            gridDivs.addEventListener('mouseover', function () {
+                gridDivs.className = 'gridDivsHover';
+            });
+
         }
-    }
+
+    }   
+}
+
+});
 
 
-   
+
+
+
+
+
+
+
+
+
+
 
 
